0fac58a36830aace220bef2e567f0027e6c7fe68
IIVR <jy.raboud@protonmail.ch>
2020-10-01 17:58:37 +0000

    add class project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,14 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
+class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
-
-    public function project()
+    public function tasks()
     {
-        return $this->belongsTo('App\Models\Project');
+        return $this->hasMany('App\Models\Task');
     }
 }
