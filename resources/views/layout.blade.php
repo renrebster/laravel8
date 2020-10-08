522496abcf80f58ee525cd6505ccadd2b36256db
IIVR <jy.raboud@protonmail.ch>
2020-10-08 19:03:07 +0000

    add debug bar and materialize CSS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,15 @@
 <html>
     <head>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </head>
     <body>
         <div class="menu">
             @yield('menu')
         </div>
 
-        <div class="content">
+        <div class="container mx-auto">
             @yield('content')
         </div>
 
