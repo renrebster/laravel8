04eb6f9e66c66d8050347a420351a5d2a8016453
IIVR <jy.raboud@protonmail.ch>
2020-10-08 17:46:07 +0000

    db heroku modif

--- config/database.php
+++ config/database.php
@@ -93,14 +93,16 @@ return [
 
          'heroku' => [
             'driver'   => 'pgsql',
-            'host'     => !is_null(getenv("DATABASE_URL")) ?: parse_url(getenv("DATABASE_URL"))["host"],
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => !is_null(getenv("DATABASE_URL")) ?: parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => !is_null(getenv("DATABASE_URL")) ?: parse_url(getenv("DATABASE_URL"))["pass"],
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
         ],
+
+
 
     ],
 
