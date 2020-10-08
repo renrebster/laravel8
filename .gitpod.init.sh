043960e9fd8bee3ae231864d18b430c5015baa32
IIVR <jy.raboud@protonmail.ch>
2020-10-08 17:06:26 +0000

    modif gitpod.init

--- .gitpod.init.sh
+++ .gitpod.init.sh
@@ -1,6 +1,6 @@
 mysql -u root -e "create database laravel"
 cp .env.example .env
-sed -i "s|APP_URL=http://localhost|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
+sed -i "s|APP_URL=http://localhost|APP_URL=8000-${GITPOD_WORKSPACE_URL}|g" .env
 composer install
 npm install
 php artisan key:generate
