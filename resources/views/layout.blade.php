46cc7ec3a7a3547c4f7b0d73b84a21ad9299cad5
IIVR <jy.raboud@protonmail.ch>
2020-10-25 08:04:23 +0000

    add css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,10 +1,16 @@
 <html>
     <head>
 
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
     </head>
     <body>
+
+        <h3>Test</h3>
         <div class="menu">
             @yield('menu')
         </div>
